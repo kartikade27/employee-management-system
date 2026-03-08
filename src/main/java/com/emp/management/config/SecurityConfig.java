e8c1c36d1341c0cb7c1a075d96a1a111f652da95
kartikade27 <kartikade27@gmail.com>
2026-03-08 11:49:58 +0530

    Updated Spring Security config

--- src/main/java/com/emp/management/config/SecurityConfig.java
+++ src/main/java/com/emp/management/config/SecurityConfig.java
@@ -86,7 +86,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsFilter() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://kartikade27.github.io/employee-management-frontend"));
+        configuration.setAllowedOrigins(List.of("https://kartikade27.github.io","http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
